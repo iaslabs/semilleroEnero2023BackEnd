da3b0f0028ab50f87b53bec847b0298e9188c3fc
Rchavem <rchave2801@gmail.com>
2022-12-05 16:15:09 -0500

    Base project

---
+++ src/main/java/com/ias/architecture/iasTraining/IasTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.ias.architecture.iasTraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IasTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IasTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ias/architecture/iasTraining/IasTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ias.architecture.iasTraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IasTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
